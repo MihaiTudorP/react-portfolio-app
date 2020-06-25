a8917a7c936069041982f2fa0979023c754e9a4a
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-06-25 21:25:22 +0300

    Refactor to use stateless functional components

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,38 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PROJECTS from '../data/projects';
 
-class Project extends Component{
-    render(){
-        console.log(this.props);
-        const {title, image, description, link} = this.props.project;
-        return (
-            <div style = {{display: 'inline-block', width: 300, margin: 10}}>
-                <h3>{title}</h3>
-                <img src={image} alt='project' style={{width: 200, height: 100}}/>
-                <p>{description}</p>
-                <a href={link}>{link}</a>
-            </div>
-        )
-    }
+const Project = props => {
+    const {title, image, description, link} = props.project;
+    return (
+        <div style = {{display: 'inline-block', width: 300, margin: 10}}>
+            <h3>{title}</h3>
+            <img src={image} alt='project' style={{width: 200, height: 100}}/>
+            <p>{description}</p>
+            <a href={link}>{link}</a>
+        </div>
+    );
 }
 
-class Projects extends Component {
-    render(){
-        return (
-            <div>
-                <h2>Highlighted projects</h2>
-                <div>
-                    {
-                        PROJECTS.map(project => {
-                            return (
-                                <Project key = {project.id} project = {project} />
-                            );
-                        })
-                    }
-                </div>
-            </div>
-        )
-    }
-}
+const Projects = () => (
+    <div>
+        <h2>Highlighted projects</h2>
+        <div>
+            {
+                PROJECTS.map(project => {
+                    return (
+                        <Project key = {project.id} project = {project} />
+                    );
+                })
+            }
+        </div>
+    </div>
+)
 
 export default Projects;
